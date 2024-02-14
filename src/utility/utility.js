a93d76e6086b083b19e180d2bb160f000776aa8f
FuutsuFIX <fpastori135@gmail.com>
2024-02-14 17:57:30 +0100

    test

--- src/utility/utility.js
+++ src/utility/utility.js
@@ -2,10 +2,11 @@ import Unknown from "../assets/images/unknown_data.png";
 
 export const getMonsterImage = (id, name) => {
     if (name) {
-        return `/src/assets/images/monsters/${name.replace(/ /g, "_")}.png`;
+        return `https://github.com/FuutsuFIX/DQM_Bestiary/blob/main/src/assets/images/monsters/${name.replace(/ /g, "_")}.png`;
     } else {
 
-        return '/src/assets/images/unknown_data.png';
+        return '../assets/images/unknown_data.png';
+
     }
 };
 
